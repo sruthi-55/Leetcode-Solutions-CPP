06c6c71ce30f87082cf4bc3388953dd97a3d445e
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-07-23 11:36:10 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

---
+++ CPP/problems/isomorphic_strings/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        //mapping characters to same value
+        unordered_map<char,int> m1,m2;
+        for(int i=0;i<s.length();i++){
+            if(m1[s[i]]!=m2[t[i]])  return false;
+            m1[s[i]]=i+1;
+            m2[t[i]]=i+1;
+        }
+        return true;
+    }
+};
