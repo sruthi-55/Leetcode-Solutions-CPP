50cece6e0e957a85fa49b7d4b5ee82591aa98462
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-08-07 14:19:29 +0000

    [LeetCode Sync] Runtime - 3 ms (68.05%), Memory - 6 MB (100.00%)

---
+++ CPP/0633-sum-of-square-numbers/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(int a=0;a<=sqrt(c);a++){
+            double b=sqrt(c-a*a);
+            if(b==(int)b)   return true;
+        }
+        return false;
+    }
+};
