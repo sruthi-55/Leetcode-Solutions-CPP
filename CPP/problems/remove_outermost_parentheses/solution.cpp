1ab851f055abe4d10f0437b54bcde7a1ef73a025
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-09-13 10:00:31 +0000

    Sync LeetCode submission - Remove Outermost Parentheses (cpp)

--- CPP/problems/remove_outermost_parentheses/solution.cpp
+++ CPP/problems/remove_outermost_parentheses/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        //keeping track of opened
-        //opened --> represent no. of opened without closed 
-        //"(()())(())"
-        // "((" --> opened=2; 
-        // "(()"  --> opened=1; 
         string ans;
         int opened=0;
-        for(auto i:s){
-            if(i=='(' && opened++>0)    ans+=i;
-            if(i==')' && opened-->1)    ans+=i;    
+        for(auto c:s){
+            if(c=='('){
+                if(opened>=1)   ans+=c;
+                opened++;
+            }  
+            else if(c==')'){
+                if(opened>1)    ans+=c;
+                opened--;
+            }
         }
         return ans;
     }
