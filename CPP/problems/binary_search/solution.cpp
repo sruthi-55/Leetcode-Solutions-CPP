c7fe8c39cb7dca742e865da06d49ac69b1dd4bf7
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-09-17 02:56:21 +0000

    Sync LeetCode submission - Binary Search (cpp)

--- CPP/problems/binary_search/solution.cpp
+++ CPP/problems/binary_search/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+    int bS(int l,int h,vector<int> &nums,int target){
+        if(l>h) return -1;
+        int mid=(l+h)/2;
+        if(nums[mid]==target)   return mid;
+        else if(nums[mid]>target)   return bS(l,mid-1,nums,target);
+        return bS(mid+1,h,nums,target);
+    }
     int search(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]==target)   return mid;
-            else if(nums[mid]>target)   h=mid-1;
-            else    l=mid+1;
-        }
-        return -1;
+        return bS(0,nums.size()-1,nums,target);
     }
 };
