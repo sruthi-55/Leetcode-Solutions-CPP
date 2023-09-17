25d3ac229e2fc298d2791d5b75e1888992aad025
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-09-17 03:54:26 +0000

    [LeetCode Sync] Runtime - 3 ms (88.97%), Memory - 14.1 MB (100.00%)

---
+++ CPP/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findFirst(vector<int>& arr, int n, int k){
+        int ans=-1;
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]==k){
+                ans=mid;
+                h=mid-1;
+            }   
+            else if(arr[mid]>k) h=mid-1;
+            else l=mid+1;
+        }
+        return ans;
+    }
+    int findLast(vector<int>& arr, int n, int k){
+        int ans=-1;
+        int l=0,h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]<=k){
+                ans=mid;
+                l=mid+1;
+            }
+            else if(arr[mid]>k) h=mid-1;
+            else    l=mid+1;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res;
+        int first = findFirst(nums,nums.size(),target);
+        if(first == -1)     return {-1,-1};
+        int last = findLast(nums,nums.size(),target);
+        return {first,last};
+    }
+};
+
+
