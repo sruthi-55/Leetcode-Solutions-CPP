4bf7f78c4f7be81edaea99e65957e51b8e3050e3
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-09-28 05:40:24 +0000

    [LeetCode Sync] Runtime - 30 ms (67.38%), Memory - 18.4 MB (98.04%)

--- CPP/0030-substring-with-concatenation-of-all-words/solution.cpp
+++ CPP/0030-substring-with-concatenation-of-all-words/solution.cpp
@@ -2,41 +2,52 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> ans;
-        int wordLen=words[0].size();
-        int noOfWords=words.size();
-        int totalLen= noOfWords*wordLen;
+        int numOfWords = words.size();
+        int wordLen = words[0].size();
+        int totalLen = numOfWords * wordLen;
 
-        if(totalLen >s.size())  return ans;
+        if(totalLen > s.size()) return ans;
+        unordered_map<string, int> count;
+        for (auto word : words) count[word]++;
 
-        unordered_map<string,int> count;
-        for(auto word:words)    count[word]++;
+        for (int i = 0; i < wordLen; i++) {
+            unordered_map<string, int> seen;
+            int left = i;
+            //j traverses for each word 
+            for (int j = i; j <= s.size() - wordLen; j += wordLen) {
+                string newWord = s.substr(j, wordLen);
 
-        int j=0;
-        for(int i=0;i<wordLen;i++){
-            unordered_map<string,int> m;
-            int left=i;
-            for(int j=i;j<=s.size()-wordLen;j+=wordLen){
-                string newWord= s.substr(j,wordLen);
-
-                if(count.find(newWord)!=count.end()){
-                    m[newWord]++;
-                    //sliding window
-                    while(m[newWord]>count[newWord]){
-                        m[s.substr(left,wordLen)]--;
-                        left+=wordLen;
+                if (count.find(newWord) != count.end()) {
+                    //add newWord to seen
+                    seen[newWord]++;
+                    //if more occurences found than what is needed 
+                    //shrink sliding window from left
+                    if (seen[newWord] > count[newWord]) {
+                        while (seen[newWord] > count[newWord]) {
+                            seen[s.substr(left, wordLen)]--;
+                            left += wordLen;
+                        }
                     }
-                    if(j+wordLen-left==totalLen){
+                    //if new window size is equal to totalLen
+                    //it means all words are found
+                    if (j + wordLen - left == totalLen) {
                         ans.push_back(left);
-                        m[s.substr(left,wordLen)]--;
-                        left+=wordLen;
-                    }  
-                }
-                else{
-                    m.clear();
-                    left=j+wordLen;
+                        //check from next word
+                        seen[s.substr(left, wordLen)]--;
+                        left += wordLen;
+                    }
+                } 
+                //if word not matched 
+                else {
+                    seen.clear();
+                    //restart search from next word
+                    left = j + wordLen;
                 }
             }
         }
         return ans;
     }
 };
+
+
+
