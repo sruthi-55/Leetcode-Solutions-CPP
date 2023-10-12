31d21c7c738712aba1099acd8981cd8b96e79c52
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-10-12 14:30:17 +0000

    Sync LeetCode submission - Palindromic Substrings (cpp)

--- CPP/problems/palindromic_substrings/solution.cpp
+++ CPP/problems/palindromic_substrings/solution.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
+    bool isPalindrome(int i,int j,string s){
+        while(i<j){
+            if(s[i]!=s[j])  return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
     int countSubstrings(string s) {
-        int n=s.size();
         int count=0;
-        vector<vector<bool>> dp(n,vector<bool>(n,false));
-        //     j   0   1   2   3   4   5   6
-        //  i      r   a   c   e   c   a   r
-        //  0  r   T   F   F   F   F   F   #
-        //  1  a       T   F   F   F   #   F
-        //  2  c           T   F   T   F   F
-        //  3  e               T   F   F   F
-        //  4  c                   T   F   F
-        //  5  a                       T   F
-        //  6  r                           T
-
-        for(int diff=0;diff<n;diff++){
-            for(int i=0,j=diff;j<n;i++,j++){
-                if(diff==0) dp[i][j]=true;
-                else if(diff==1 && s[i]==s[j])  dp[i][j]=true;
-                else{
-                    if(s[i]==s[j])
-                        dp[i][j]=dp[i+1][j-1];
-                }
-                if(dp[i][j])  count++;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,s)) count++;
             }
         }
         return count;
