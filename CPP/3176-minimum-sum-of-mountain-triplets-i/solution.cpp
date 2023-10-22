1e465e0908e1b0829821753b5bf6903b135c8424
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-10-22 02:39:05 +0000

    [LeetCode Sync] Runtime - 9 ms (23.23%), Memory - 21 MB (100.00%)

---
+++ CPP/3176-minimum-sum-of-mountain-triplets-i/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+        int n=nums.size();
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]<nums[j] && nums[k]<nums[j])
+                        ans=min(ans,nums[i]+nums[j]+nums[k]);
+                }
+            }
+        }
+        if(ans!=INT_MAX)    return ans;
+        return -1;
+    }
+};
