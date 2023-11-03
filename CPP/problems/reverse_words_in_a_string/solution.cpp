0fe0701cd7977881f07f44018d3750171f295f58
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-11-03 07:31:57 +0000

    Sync LeetCode submission - Reverse Words in a String (cpp)

--- CPP/problems/reverse_words_in_a_string/solution.cpp
+++ CPP/problems/reverse_words_in_a_string/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        string res;
-        int n=s.length();
-        for(int i=0;i<n;){
-            while(i<n && s[i]==' ') i++;
-            int j=i;
-            while(j<n && s[j]!=' ') j++;
-            if(i>=n)    break; 
+    string reverseWords(string s){
+        string res="";
+        int i=0,j=0;
+        while(j<s.size()){
+            //i points to first letter
+            while(i<s.size() && s[i]==' ')  i++;
+            //j points to space after word
+            j=i;
+            while(j<s.size() && s[j]!=' ')  j++;
+            if(i>=s.size())    break; 
             
             if(res.length()==0) res=s.substr(i,j-i);
             else    res=s.substr(i,j-i)+" "+res;
             i=j;
         }
         return res;
-        
-        // , ,h,e,l,l,o, ,w,o,r,l,d, , //
-        //res=world hello
     }
 };
