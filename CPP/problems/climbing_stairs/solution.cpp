0f4ee6f33c68de053e055f3b27d07bdba3f0ac34
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-11-05 05:54:08 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- CPP/problems/climbing_stairs/solution.cpp
+++ CPP/problems/climbing_stairs/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int f(int i,vector<int> &dp){
-        if(i==0 || i==1)    return 1;
-        if(dp[i]!=-1)   return dp[i];
-        return dp[i]= f(i-1,dp) + f(i-2,dp);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
+        int dp[n+1];
+        dp[0] = 1; 
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
