093af8a30c27706d41d10192c6fe063dbaa406b4
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-12-03 06:47:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.9 MB (100.00%)

--- CPP/0035-search-insert-position/solution.cpp
+++ CPP/0035-search-insert-position/solution.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1;
-        int ans=nums.size();
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(nums[mid]==target)   return mid;
-            else if(nums[mid]>target){
-                ans=mid;
-                h=mid-1;
-            }
-            else    l=mid+1;
-        }
-        return ans;
+        //need to find index of ele >=target
+        //lower bound
+        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     }
 };
