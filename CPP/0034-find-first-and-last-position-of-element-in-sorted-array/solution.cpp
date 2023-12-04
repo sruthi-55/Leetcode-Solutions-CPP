5fbe7e28dd13d0a617456b58d96a5965c16f5309
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-12-04 15:53:14 +0000

    [LeetCode Sync] Runtime - 3 ms (88.97%), Memory - 13.9 MB (100.00%)

--- CPP/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ CPP/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,38 +1,13 @@
 class Solution {
 public:
-    int findFirst(vector<int>& arr, int n, int k){
-        int ans=-1;
-        int l=0,h=n-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(arr[mid]==k){
-                ans=mid;
-                h=mid-1;
-            }   
-            else if(arr[mid]>k) h=mid-1;
-            else l=mid+1;
-        }
-        return ans;
-    }
-    int findLast(vector<int>& arr, int n, int k){
-        int ans=-1;
-        int l=0,h=n-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(arr[mid]<=k){
-                ans=mid;
-                l=mid+1;
-            }
-            else if(arr[mid]>k) h=mid-1;
-            else    l=mid+1;
-        }
-        return ans;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> res;
-        int first = findFirst(nums,nums.size(),target);
-        if(first == -1)     return {-1,-1};
-        int last = findLast(nums,nums.size(),target);
-        return {first,last};
+        int ind1 = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(ind1>=nums.size() || nums[ind1]!=target)   return {-1,-1};
+        res.push_back(ind1);
+
+        int ind2 = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        res.push_back(ind2-1);
+        return res;
     }
 };
