b8b2d2f20a5d778ce07b5ce1de1e41b21639caef
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-12-04 15:38:14 +0000

    [LeetCode Sync] Runtime - 10 ms (73.31%), Memory - 16.1 MB (100.00%)

--- CPP/0745-find-smallest-letter-greater-than-target/solution.cpp
+++ CPP/0745-find-smallest-letter-greater-than-target/solution.cpp
@@ -2,19 +2,9 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int n=letters.size();
-        int l=0,h=n-1;
-        int ans=letters[0];
-        while(l<=h){
-            int mid=(l+h)/2;
-            //greater than target
-            if(letters[mid]>target){
-                //this could be ans
-                ans=letters[mid];
-                //to find smallest letter we move to left subarray
-                h=mid-1;
-            }
-            else    l=mid+1;
-        }
+        char ans=letters[0];
+        int ind = upper_bound(letters.begin(),letters.end(),target)-letters.begin();
+        if(ind<n)   ans=letters[ind];
         return ans;
     }
 };
