a56bd99425c33c52e51cc4701073935197c4a741
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-12-05 04:58:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.3 MB (100.00%)

--- CPP/0033-search-in-rotated-sorted-array/solution.cpp
+++ CPP/0033-search-in-rotated-sorted-array/solution.cpp
@@ -4,16 +4,16 @@ public:
         int l=0,h=nums.size()-1;
         while(l<=h){
             int mid=(l+h)/2;
-            if(nums[mid]==target)   return mid;
-            //left part is sorted
-            else if(nums[l]<=nums[mid]){
-                if(nums[l]<=target && target<=nums[mid]) h=mid-1;
-                else    l=mid+1;
-            }
+            if(target==nums[mid])   return mid;
             //right part is sorted
-            else{
-                if(nums[mid]<=target && target<=nums[h])  l=mid+1;
+            else if(nums[mid]<=nums[h]){
+                if(nums[mid]<=target && target<=nums[h])    l=mid+1;
                 else    h=mid-1;
+            }
+            //left part is sorted
+            else{
+                if(nums[l]<=target && target<=nums[mid])    h=mid-1;
+                else    l=mid+1;
             }
         }
         return -1;
