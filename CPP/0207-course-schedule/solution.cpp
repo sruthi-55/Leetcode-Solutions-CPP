16fd949d6f0b7bbed4a292a5183d21ec533fe236
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2023-12-10 16:12:35 +0000

    [LeetCode Sync] Runtime - 16 ms (58.19%), Memory - 15 MB (100.00%)

--- CPP/0207-course-schedule/solution.cpp
+++ CPP/0207-course-schedule/solution.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
+    bool hasCycle(int i,vector<int> &visited,vector<int> &pathVisited,
+vector<vector<int>> &adj){
+        visited[i]=1;
+        pathVisited[i]=1;
+        for(auto nei:adj[i]){
+            if(!visited[nei]){
+                if(hasCycle(nei,visited,pathVisited,adj))	return true;
+            }
+            else if(pathVisited[nei]==1)	return true;
+        }
+        pathVisited[i]=0;
+        return false;
+    }
     bool canFinish(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
         for(auto e:edges){
             adj[e[1]].push_back(e[0]);
         }
 
-        vector<int> inDegree(n,0);
+        vector<int> visited(n,0);
+        vector<int> pathVisited(n,0);
         for(int i=0;i<n;i++){
-            for(auto nei:adj[i]){
-                inDegree[nei]++;
+            if(!visited[i]){
+                if(hasCycle(i,visited,pathVisited,adj))	return false;
             }
         }
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(inDegree[i]==0)  q.push(i);
-        }
-        int count=0;
-        while(!q.empty()){
-            int node=q.front(); q.pop();
-            count++;
-            for(auto nei:adj[node]){
-                inDegree[nei]--;
-                if(inDegree[nei]==0)    q.push(nei);
-            }
-        }
-        return count==n ? true : false;
+        return true;
     }
 };
