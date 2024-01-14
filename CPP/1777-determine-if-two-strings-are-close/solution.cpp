e81686554735840a66b34b53a3172b7399a249f5
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-01-14 13:14:11 +0000

    [LeetCode Sync] Runtime - 146 ms (9.34%), Memory - 21.2 MB (99.15%)

--- CPP/1777-determine-if-two-strings-are-close/solution.cpp
+++ CPP/1777-determine-if-two-strings-are-close/solution.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    bool closeStrings(string s1, string s2) {
+    bool closeStrings(string word1, string word2) {
         //HINT:
         //Operation 1 allows you to freely reorder the string.
-        // ----> strings should have same number of unique letters
+        // ----> strings should have same unique letters
         //Operation 2 allows you to freely reassign the letters' frequencies.
         // ----> sorted frequencies are equal
-        vector<int> freq1(26), freq2(26);
-        vector<int> unique1(26), unique2(26);
-        for (char c : s1) {
-            unique1[c - 'a'] = 1;
-            freq1[c - 'a']++;
+        if(word1.size()!=word2.size())  return false;
+
+        vector<int> freq1(26),freq2(26);
+        set<char> uniq1,uniq2;
+        for(int i=0;i<word1.size();i++){
+            char c1=word1[i],c2=word2[i];
+
+            uniq1.insert(c1);
+            freq1[c1-'a']++;
+
+            uniq2.insert(c2);
+            freq2[c2-'a']++;
         }
-        for (char c : s2) {
-            unique2[c - 'a'] = 1;
-            freq2[c - 'a']++;
-        }
-        if (unique1 != unique2) return false;
-        sort(freq1.begin(), freq1.end());
-        sort(freq2.begin(), freq2.end());
-        return freq1 == freq2;
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+        
+        return uniq1==uniq2 && freq1==freq2;
     }
 };
