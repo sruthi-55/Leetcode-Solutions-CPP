ed77dd6a4f75fcbb91de3c7b769bbbc9bf9cb6de
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-02-15 04:50:58 +0000

    [LeetCode Sync] Runtime - 20 ms (72.59%), Memory - 28.3 MB (80.42%)

--- CPP/1019-squares-of-a-sorted-array/solution.cpp
+++ CPP/1019-squares-of-a-sorted-array/solution.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n=nums.size();
-        vector<int> res(n);
+
         int l=0,h=n-1;
+        vector<int> res(n);
         int k=n-1;
         while(l<=h){
-            if(abs(nums[l])>=abs(nums[h]))   res[k--]=nums[l]*nums[l],l++;
-            else    res[k--]=nums[h]*nums[h],h--;
+            if(abs(nums[l])<=abs(nums[h])){
+                res[k--]=nums[h]*nums[h];
+                h--;
+            }
+            else{
+                res[k--]=nums[l]*nums[l];
+                l++;
+            }    
         }
         return res;
     }
