7114c30420b2842deb659eb8e5fe123b6a3d554a
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-02-15 14:29:00 +0000

    [LeetCode Sync] Runtime - 5 ms (21.70%), Memory - 13.4 MB (19.92%)

--- CPP/0019-remove-nth-node-from-end-of-list/solution.cpp
+++ CPP/0019-remove-nth-node-from-end-of-list/solution.cpp
@@ -11,24 +11,17 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head || !head->next)   return NULL;
-        ListNode *temp=head;
-        ListNode *before=head;
-        //find length of LL
-        int length=0;
-        while(temp){
-            temp=temp->next;
-            length++;
-        }
-        
-        //if head is to be deleted
-        if(n==length)   return head->next;
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *left = dummy, *right = head;
+        while(n-- && right) 
+            right=right->next;
 
-        //k will be the index of node just before the node to be deleted
-        int k=length-n-1;
-        while(k-- && before->next)
-            before=before->next;
-        before->next=before->next->next;
-        return head;
-    }    
+        while(right){
+            left = left->next;
+            right = right->next;
+        }
+        left->next = left->next->next;
+        return dummy->next;
+    }
 };
