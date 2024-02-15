0a241cd4f1481fa6641c4209ae2046f732e1e55b
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-02-15 04:57:17 +0000

    [LeetCode Sync] Runtime - 4 ms (78.71%), Memory - 18.8 MB (53.76%)

--- CPP/0941-sort-array-by-parity/solution.cpp
+++ CPP/0941-sort-array-by-parity/solution.cpp
@@ -3,8 +3,12 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int l=0,h=nums.size()-1;
         while(l<h){
-            if(nums[h]%2==0)    swap(nums[l++],nums[h]);
-            else    h--;
+            while(l<h && nums[l]%2==0) l++;
+            while(l<h && nums[h]%2==1) h--;
+            int temp = nums[l];
+            nums[l]=nums[h];
+            nums[h]=temp;
+            l++;h--;
         }
         return nums;
     }
