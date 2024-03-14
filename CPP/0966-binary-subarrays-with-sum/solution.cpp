ce732d03fab0da0b7a0ded807fc2acc20e807aeb
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-03-14 14:46:46 +0000

    [LeetCode Sync] Runtime - 39 ms (30.91%), Memory - 38 MB (29.51%)

---
+++ CPP/0966-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        unordered_map<int, int> prefixSum;
+        
+        int count   = 0;
+        int currSum = 0;
+        prefixSum[0] = 1;
+        for(int &num : nums) {
+            currSum += num;
+            int req = currSum - goal;
+            if(prefixSum.find(req) != prefixSum.end()) {
+                count += prefixSum[req];
+            }
+            
+            prefixSum[currSum]++;
+        }
+        return count;
+    }
+};
+
