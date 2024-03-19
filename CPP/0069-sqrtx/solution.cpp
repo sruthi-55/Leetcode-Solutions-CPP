bc39cc58d92c3a3d853e4c328ce739a055fe3d0b
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-03-19 06:07:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.5 MB (39.36%)

--- CPP/0069-sqrtx/solution.cpp
+++ CPP/0069-sqrtx/solution.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int l=0,h=x;
-        int ans=-1;
-        while(l<=h){
-            long mid=l+(h-l)/2;
-            if(mid*mid==x)  return mid;
-            else if(mid*mid<x){
+        return bs(0,x,x);
+    }
+    int bs(int low,int high,int target){
+        int ans = -1;
+        while(low<=high){
+            long mid = low + (high - low)/2;
+            if(mid*mid==target){
                 ans=mid;
-                l=mid+1;
+                break;
             }
-            else    h=mid-1;
+            else if(mid*mid<target){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return ans;
     }
