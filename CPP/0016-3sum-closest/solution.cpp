d1b0b60ceec817a2c53b08f73d92300a16332335
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-03-19 04:19:08 +0000

    [LeetCode Sync] Runtime - 15 ms (63.87%), Memory - 12.8 MB (65.25%)

--- CPP/0016-3sum-closest/solution.cpp
+++ CPP/0016-3sum-closest/solution.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         
-        for(int i=0;i<=n-3;i++){
+        for(int i=0;i<n;i++){
             int l=i+1,h=n-1;
             while(l<h){
                 int sum = nums[i]+nums[l]+nums[h];
