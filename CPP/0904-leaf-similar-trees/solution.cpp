4e18c5e4463c399c44e2c31e8a008f8f62fcd26c
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-04-22 02:22:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.8 MB (13.74%)

--- CPP/0904-leaf-similar-trees/solution.cpp
+++ CPP/0904-leaf-similar-trees/solution.cpp
@@ -11,23 +11,25 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root,vector<int>& v){
-        if(!root)   return ;
-        if(!root->left && !root->right)   v.push_back(root->val);
+    void dfs(TreeNode* root, string& s) {
+        if(root == NULL) return;
         
-        dfs(root->left,v);
-        dfs(root->right,v);
-    }
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> v1,v2;
-        dfs(root1,v1);
-        dfs(root2,v2);
-        
-        if(v1.size()!=v2.size())    return false;
-
-        for(int i=0;i<v1.size();i++){
-            if(v1[i]!=v2[i])    return false;
+        if(root->left == NULL && root->right == NULL) {
+            s += to_string(root->val) + "_";
+            return;
         }
-        return true;
+        
+        dfs(root->left, s);
+        dfs(root->right, s);
+    }
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        string s1 = "";
+        string s2 = "";
+        
+        dfs(root1, s1);
+        dfs(root2, s2);
+        
+        return s1==s2;
     }
 };
