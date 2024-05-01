6fc76aa19215a9b4d3b38359502b7ecf50bfa8c8
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-05-01 02:53:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.5 MB (65.11%)

--- CPP/2128-reverse-prefix-of-word/solution.cpp
+++ CPP/2128-reverse-prefix-of-word/solution.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void reverse(string &word,int ind){
-        int l=0,h=ind;
-        while(l<h){
-            int temp=word[l];
-            word[l]=word[h];
-            word[h]=temp;
-            l++;h--;
+    void reverse(string &word,int s,int e){
+        while(s<=e){
+            char temp=word[s];
+            word[s]=word[e];
+            word[e]=temp;
+            s++;
+            e--;
         }
     }
     string reversePrefix(string word, char ch) {
-        int ind=-1;
-        for(int i=0;i<word.size();i++){
+        int n=word.size();
+        int ind = -1;
+        for(int i=0;i<n;i++){
             if(word[i]==ch){
-                ind=i;break;
-            }
+                ind = i;
+                break;
+            }  
         }
-        if(ind!=-1)
-            reverse(word,ind);
+        if(ind==-1) return word;
+        reverse(word,0,ind);
         return word;
     }
 };
