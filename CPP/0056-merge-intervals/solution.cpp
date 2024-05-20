4af15be2556dea843775803151bc7896ff675396
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-05-20 06:12:01 +0000

    [LeetCode Sync] Runtime - 15 ms (96.84%), Memory - 22.8 MB (41.71%)

---
+++ CPP/0056-merge-intervals/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end());
+
+        int start=intervals[0][0],end=intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=end)
+                end = max(end,intervals[i][1]);
+            else{
+                res.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        res.push_back({start,end});
+        return res;
+    }
+};
