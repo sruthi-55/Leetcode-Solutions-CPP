bee07cf47675c2c4c92fef90b6715cbe892016fa
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-05-22 05:56:06 +0000

    [LeetCode Sync] Runtime - 3 ms (66.36%), Memory - 11.8 MB (98.53%)

--- CPP/0074-search-a-2d-matrix/solution.cpp
+++ CPP/0074-search-a-2d-matrix/solution.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    bool binarySearch(vector<int> &nums,int target){
-        int l=0,h=nums.size()-1;
-        while(l<=h){
-            int mid = (l+h)/2;
-            if(nums[mid]==target)   return true;
-            else if(nums[mid]>target)   h=mid-1;
-            else    l=mid+1;
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size(),m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]<=target && target<=matrix[i][m-1])
-                return binarySearch(matrix[i],target);
+        int l = 0, h = n*m-1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            int r = mid/m;
+            int c = mid%m;
+            if(matrix[r][c]==target)    return true;
+            else if(matrix[r][c]<target)    l=mid+1;
+            else    h=mid-1;
         }
         return false;
     }
