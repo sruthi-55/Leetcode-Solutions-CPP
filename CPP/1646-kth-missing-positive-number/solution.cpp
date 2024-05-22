568632d1591231790702d0e1ed19f2fc3a13facf
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-05-22 04:50:29 +0000

    [LeetCode Sync] Runtime - 4 ms (40.32%), Memory - 12.1 MB (40.42%)

--- CPP/1646-kth-missing-positive-number/solution.cpp
+++ CPP/1646-kth-missing-positive-number/solution.cpp
@@ -4,7 +4,8 @@ public:
         int l=0,h=arr.size()-1;
         while(l<=h){
             int mid=(l+h)/2;
-            if(arr[mid]-mid <= k){
+            int missing = arr[mid]-(mid+1);
+            if(missing < k){
                 l=mid+1;
             }
             else    h=mid-1;
