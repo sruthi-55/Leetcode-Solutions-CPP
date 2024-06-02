edd18392c4c21e4683d388b9042811401159b3d1
Sruthi <121864217+sruthi-55@users.noreply.github.com>
2024-06-02 03:43:43 +0000

    [LeetCode Sync] Runtime - 15 ms (75.42%), Memory - 27.1 MB (21.10%)

--- CPP/0344-reverse-string/solution.cpp
+++ CPP/0344-reverse-string/solution.cpp
@@ -3,10 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int l=0,h=s.size()-1;
         while(l<h){
-            char temp=s[l];
-            s[l]=s[h];
-            s[h]=temp;
-            l++;h--;
+            swap(s[l++],s[h--]);
         }
     }
 };
